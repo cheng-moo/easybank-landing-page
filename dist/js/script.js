08897b9a699598801b54942b83f602b552837e48
Cheng_Mo_ <chengmooooo@gmail.com>
2022-05-01 10:54:50 +0200

    create and animate mobile menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,25 @@
 const btnHamburger = document.querySelector('#btnHamburger')
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function() {
     if(header.classList.contains('open')) { // Close Hamburger Menu
     header.classList.remove('open');
+    body.classList.remove('noscroll');
+    fadeElems.forEach(element => {
+        element.classList.add('fade-out');
+        element.classList.remove('fade-in');
+    })
     overlay.classList.remove('fade-in');
     overlay.classList.add('fade-out');
     } else { // Open Hamburger Menu
-    header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+        body.classList.add('noscroll')
+        header.classList.add('open');
+        fadeElems.forEach(element => {
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
+    } )
     }
 })
